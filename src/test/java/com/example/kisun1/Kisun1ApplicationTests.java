19683a03feec0779e8ec0757d2c4fcee09e2760b
seinyang <did756984@naver.com>
2024-03-21 21:06:00 +0900

    스프링부트 개념과 활용 (백기선_

---
+++ src/test/java/com/example/kisun1/Kisun1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kisun1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kisun1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
